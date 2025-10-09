36c13aba2350507346030bee736fb62d32d225d6
JK Nordstrrand <nordstrand@MacBook-Pro-som-tillhor-JK.local>
2025-10-09 15:18:43 +0200

    add todo logicc

---
+++ app.js
@@ -0,0 +1,29 @@
+const todoForm = document.querySelector("#todo-form")
+const todoInput = document.querySelector("#todo-input")
+const todoList = document.querySelector("#todo-list")
+
+
+const addTodo = text => {
+    if (!text.trim() ) {
+        return
+    }
+
+    const li = document.createElement ("li")
+    li.textContent = text
+
+    todoList.appendChild(li)
+}
+
+    todoForm.addEventListener("submit", e =>  {
+    e.preventDefault()
+
+    addTodo todoInput.value
+
+    todoForm.reset()
+
+})
+
+
+
+
+
