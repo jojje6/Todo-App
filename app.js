2a40525e1d7b42c703c1401023d0581445722278
JK Nordstrand <joohannees@hotmail.se>
2025-10-15 16:32:46 +0200

    Update HTML, CSS, and JS for Todo App

--- app.js
+++ app.js
@@ -1,29 +1,55 @@
-const todoForm = document.querySelector("#todo-form")
-const todoInput = document.querySelector("#todo-input")
-const todoList = document.querySelector("#todo-list")
+    const todoForm = document.getElementById("todo-form");
+    const todoInput = document.getElementById("todo-input");
+    const todoList = document.getElementById("todo-list");
+
+    const addTodo = (text) => {
+        if (!text.trim()) {
+            return;
+        }
+
+        const li = document.createElement("li");
+        const deleteButton = document.createElement("button");
+        const span = document.createElement("span");
+        const checkbox = document.createElement("input");
+        const container = document.createElement("div");
 
 
-const addTodo = text => {
-    if (!text.trim() ) {
-        return
+        checkbox.type = "checkbox";
+        checkbox.className = "custom-checkbox";
+        
+        span.textContent = text;
+        span.classList = "todo-text";
+
+        deleteButton.textContent = "x";
+        deleteButton.classList = "delete-button";
+
+        container.appendChild(checkbox);
+        container.appendChild(span);
+
+        li.appendChild(container);
+        li.appendChild(deleteButton);
+
+        checkbox.addEventListener("change", (e) => {
+            if (checkbox.checked) {
+                span.style.textDecoration = "line-through";
+            } else {
+                span.style.textDecoration = "none";
+            }
+        })
+
+        deleteButton.addEventListener("click", (e) => {
+            li.remove()
+        })
+
+
+
+        todoList.appendChild(li);
+
+        todoInput.value = '';
     }
 
-    const li = document.createElement ("li")
-    li.textContent = text
+    todoForm.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    todoList.appendChild(li)
-}
-
-    todoForm.addEventListener("submit", e =>  {
-    e.preventDefault()
-
-    addTodo todoInput.value
-
-    todoForm.reset()
-
-})
-
-
-
-
-
+        addTodo(todoInput.value);
+    })
